9a1d2d3b7dd3a322120f6660ed14528914969108
Juan Castillo <Juanpablodlcc@gmail.com>
2021-04-17 17:39:06 -0600

    [ADD] renkeds queus to query

--- server/functions/index.js
+++ server/functions/index.js
@@ -19,14 +19,17 @@ const https = axios.create({
 api.get("/summoner/:tag", async (req, res) => {
     try {
         const { tag } = req.params
-        const query = await https.get(`/summoner/v4/summoners/by-name/${tag}`)
-        const summonet = query.data
-        res.send({ payload: summonet });
+        const query_account = await https.get(`/summoner/v4/summoners/by-name/${tag}`)
+        const summoner = query_account.data
+        const query_ranked = await https.get(`/league/v4/entries/by-summoner/${summoner.id}`)
+        const queues = query_ranked.data
+
+        summoner.queues = queues
+        res.send({ payload: summoner });
     } catch (err) {
         res.sendStatus(400)
     }
     res.end()
-
 })
 
 
