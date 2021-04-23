3df9db17168abbc2a57537184a6288cda564789e
Juan Castillo <Juanpablodlcc@gmail.com>
2021-04-23 13:35:08 -0600

    [ADD] tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Routes from './routes'
+import "./styles.css"
 function App() {
   return <div><Routes/></div>
 }
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,25 +1,17 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../views/home'
 import Pickem from '../views/pickem'
 import Stats from '../views/stats'
 import E404 from '../views/404'
-export default function Routes(){
-    return(
+export default function Routes() {
+    return (
         <Router>
             <Switch>
-                <Route path="/" exact>
-                    <Home/>
-                </Route>
-                <Route path="/pickem" exact>
-                    <Pickem/>
-                </Route>
-                <Route path="/stats" exact>
-                    <Stats />
-                </Route>
-                <Route path="" >
-                    <E404/>
-                </Route>
-
+                <Route exact path="/" component={Home} />
+                <Route exact path="/pickem" component={Pickem} />
+                <Route exact path="/stats" component={Stats} />
+                <Route exact path="/404" component={E404} />
+                <Redirect to="/404" ></Redirect>
             </Switch>
         </Router>
     )
